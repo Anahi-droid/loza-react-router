184e2b01837b6839c3d63de1386a68bfaa6076a1
Anahi-droid <lozaanahi02@gmail.com>
2026-07-07 07:49:21 -0500

    first commit

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,16 @@
+
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 style={{ marginTop: 12 }}>ACerca de nosotos</h1>
+      <p style={{ color: '#6b7280' }}>
+        Nuestra Empresa.
+      </p>
+      <p style={{ color: '#6b7280' }}>
+        Programacion III
+    
+      </p>
+    </div>
+  )
+}
